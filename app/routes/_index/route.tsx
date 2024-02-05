f06cc651920e072e7069f0177eb44d4ca58ab933
kaitok <kaitokinjo@gmail.com>
2024-02-05 20:38:38 +0900

    add projects

---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,25 @@
+import type { MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Projects</h1>
+
+      <ul>
+        <li>
+          <a href="projects/1">test project1</a>
+        </li>
+        <li>
+          <a href="projects/2">test project2</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
