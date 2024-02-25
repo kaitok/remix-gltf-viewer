070b931a913586f2ef3ba8b9b7b5b21892464a8c
kaitok <kaitokinjo@gmail.com>
2024-02-25 15:06:40 +0900

    display project info

--- app/routes/projects_.$projectId.tsx
+++ app/routes/projects_.$projectId.tsx
@@ -1,9 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { Outlet } from '@remix-run/react'
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const project = params.projectId
+  const prisma = new PrismaClient()
+  const project = await prisma.project.findUnique({
+    where: {
+      id: Number(params.projectId),
+    },
+  })
   return json({ project })
 }
 
@@ -11,23 +17,11 @@ export default function Project() {
   const { project } = useLoaderData<typeof loader>()
   return (
     <>
-      <h1>Project {project}</h1>
+      <h1>{project?.title}</h1>
 
-      <h2>
-        <a href={`/projects/${project}/notes`}>Notes</a>
-      </h2>
       <ul>
         <li>
-          <a href={`/projects/${project}/notes/1`}>Notes 1</a>
-        </li>
-        <li>
-          <a href={`/projects/${project}/notes/2`}>Notes 2</a>
-        </li>
-        <li>
-          <a href={`/projects/${project}/notes/3`}>Notes 3</a>
-        </li>
-        <li>
-          <a href={`/projects/${project}/notes/4`}>Notes 4</a>
+          <a href={`/projects/${project?.id}/notes/1`}>Notes 1</a>
         </li>
       </ul>
     </>
