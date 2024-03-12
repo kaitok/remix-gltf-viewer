d0a8131b52208cbf0faec5824e3d7825469c6324
kaitok <kaitokinjo@gmail.com>
2024-03-12 16:51:15 +0900

    remove font-bold

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
               href={'projects/' + v.id}
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 min-w-64"
             >
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-90">
+              <h5 className="mb-2 text-xl tracking-tight text-gray-90">
                 {v.title}
               </h5>
               <p className="font-normal text-gray-700">description</p>
