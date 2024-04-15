e9b360593abd61eff4878ad8bc9aae91f0fed13e
kaitok <kaitokinjo@gmail.com>
2024-04-15 22:21:11 +0900

    remove uuid from user

---
+++ app/services/register.server.ts
@@ -0,0 +1,21 @@
+import User from 'type/user'
+import bcrypt from 'bcrypt'
+import { prisma } from '~/db.server'
+
+export async function login(email: string, password: string): Promise<User> {
+  return await {
+    id: '1',
+    username: 'name',
+    passwordHash: 'test',
+    uuid: 'aaaaaa',
+    createdAt: 'date',
+    updatedAt: 'date',
+  }
+}
+
+export async function register(username: string, password: string) {
+  let passwordHash = await bcrypt.hash(password, 10)
+  return prisma.user.create({
+    data: { username, passwordHash },
+  })
+}
---
+++ prisma/migrations/20240414042515_uuuid/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - The primary key for the `Note` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Project` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `uuid` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Note" DROP CONSTRAINT "Note_projectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Project" DROP CONSTRAINT "Project_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Note" DROP CONSTRAINT "Note_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "projectId" SET DATA TYPE TEXT,
+ALTER COLUMN "authorId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Note_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Note_id_seq";
+
+-- AlterTable
+ALTER TABLE "Project" DROP CONSTRAINT "Project_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Project_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Project_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "name",
+DROP COLUMN "uuid",
+ADD COLUMN     "passwordHash" TEXT NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
