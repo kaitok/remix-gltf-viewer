d5af34d691febd7bd5ad2dd837d7374d36fef700
kaitok <kaitokinjo@gmail.com>
2024-04-15 23:07:01 +0900

    add signup

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -3,14 +3,16 @@ import { Form } from '@remix-run/react'
 import { authenticator } from '~/services/auth.server'
 import Button from '~/components/Button'
 
-// First we create our UI with the form doing a POST and the inputs with the
-// names we are going to use in the strategy
-export default function Screen() {
+export async function action({ request }: ActionFunctionArgs) {
+  return null
+}
+
+export default function SignUp() {
   return (
     <div className="py-5 px-8">
       <div className="max-w-xl mt-5">
         <div className="my-3">
-          <h1 className="text-xl">Login</h1>
+          <h1 className="text-xl">Sign Up</h1>
         </div>
         <Form method="post">
           <div className="mb-5">
@@ -55,26 +57,4 @@ export default function Screen() {
       </div>
     </div>
   )
-}
-
-// Second, we need to export an action function, here we will use the
-// `authenticator.authenticate method`
-export async function action({ request }: ActionFunctionArgs) {
-  // we call the method with the name of the strategy we want to use and the
-  // request object, optionally we pass an object with the URLs we want the user
-  // to be redirected to after a success or a failure
-  return await authenticator.authenticate('user-pass', request, {
-    successRedirect: '/',
-    failureRedirect: '/login',
-  })
-}
-
-// Finally, we can export a loader function where we check if the user is
-// authenticated with `authenticator.isAuthenticated` and redirect to the
-// dashboard if it is or return null if it's not
-export async function loader({ request }: LoaderFunctionArgs) {
-  // If the user is already authenticated redirect to /dashboard directly
-  return await authenticator.isAuthenticated(request, {
-    successRedirect: '/',
-  })
 }
